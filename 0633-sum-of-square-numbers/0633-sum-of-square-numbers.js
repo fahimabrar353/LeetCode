d44de3a5cb05c833d1be7c89ce65399f17500ebf
fahimabrar353 <47246463+fahimabrar353@users.noreply.github.com>
2024-06-17 21:35:01 +0600

    Time: 53 ms (84.25%), Space: 49.9 MB (52.74%) - LeetHub

---
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.js
@@ -0,0 +1,18 @@
+var judgeSquareSum = function (c) {
+    let left = 0;
+    let right = Math.floor(Math.sqrt(c));
+
+    while (left <= right) {
+        const currentSum = left * left + right * right;
+
+        if (currentSum === c) {
+            return true;
+        } else if (currentSum < c) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+
+    return false;
+};
