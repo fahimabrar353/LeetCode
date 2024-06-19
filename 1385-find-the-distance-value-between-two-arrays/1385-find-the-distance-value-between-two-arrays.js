c18bad4f4abb8b770ec46c8a6413653e5d1b9fe5
fahimabrar353 <47246463+fahimabrar353@users.noreply.github.com>
2024-06-20 04:24:37 +0600

    Time: 69 ms (24.32%), Space: 49.3 MB (75.68%) - LeetHub

---
+++ 1385-find-the-distance-value-between-two-arrays/1385-find-the-distance-value-between-two-arrays.js
@@ -0,0 +1,18 @@
+var findTheDistanceValue = function (arr1, arr2, d) {
+    let distanceValue = 0;
+
+    for (let i = 0; i < arr1.length; i++) {
+        let valid = true;
+        for (let j = 0; j < arr2.length; j++) {
+            if (Math.abs(arr1[i] - arr2[j]) <= d) {
+                valid = false;
+                break;
+            }
+        }
+        if (valid) {
+            distanceValue++;
+        }
+    }
+
+    return distanceValue;
+};
