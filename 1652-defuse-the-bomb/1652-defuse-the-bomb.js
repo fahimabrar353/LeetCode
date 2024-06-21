219567304873edf2508a7154de0241f87e613c8f
fahimabrar353 <47246463+fahimabrar353@users.noreply.github.com>
2024-06-22 02:46:06 +0600

    Time: 62 ms (42.05%), Space: 49.9 MB (46.97%) - LeetHub

---
+++ 1652-defuse-the-bomb/1652-defuse-the-bomb.js
@@ -0,0 +1,24 @@
+var decrypt = function (code, k) {
+    const n = code.length;
+    let result = new Array(n).fill(0);
+
+    if (k === 0) {
+        return result;
+    }
+
+    for (let i = 0; i < n; i++) {
+        let sum = 0;
+        if (k > 0) {
+            for (let j = 1; j <= k; j++) {
+                sum += code[(i + j) % n];
+            }
+        } else {
+            for (let j = 1; j <= -k; j++) {
+                sum += code[(i - j + n) % n];
+            }
+        }
+        result[i] = sum;
+    }
+
+    return result;
+};
