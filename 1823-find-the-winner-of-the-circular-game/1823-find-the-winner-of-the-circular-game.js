ca06b065e10bd444785e769baeee24e746d93048
fahimabrar353 <47246463+fahimabrar353@users.noreply.github.com>
2024-07-10 03:54:03 +0600

    Time: 48 ms (91.02%), Space: 48.9 MB (85.35%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.js
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.js
@@ -1,15 +1,7 @@
 var findTheWinner = function (n, k) {
-    let friends = [];
-    for (let i = 1; i <= n; i++) {
-        friends.push(i);
-    }
-    
-    let currentIndex = 0;
-
-    while (friends.length > 1) {
-        currentIndex = (currentIndex + k - 1) % friends.length;
-        friends.splice(currentIndex, 1);
-    }
-
-    return friends[0];
+    let winner = 0;
+        for (let i = 1; i <= n; i++) {
+            winner = (winner + k) % i;
+        }
+        return winner + 1;
 };
