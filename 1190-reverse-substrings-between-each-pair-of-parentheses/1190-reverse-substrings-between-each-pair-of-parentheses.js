a49becbee2917a10b653db6d3d09cf14c495ad2a
fahimabrar353 <47246463+fahimabrar353@users.noreply.github.com>
2024-07-11 21:26:10 +0600

    Time: 53 ms (79.63%), Space: 51 MB (68.52%) - LeetHub

---
+++ 1190-reverse-substrings-between-each-pair-of-parentheses/1190-reverse-substrings-between-each-pair-of-parentheses.js
@@ -0,0 +1,20 @@
+var reverseParentheses = function(s) {
+    let stack = [];
+
+    for (let char of s) {
+        if (char === ')') {
+            let temp = '';
+            while (stack.length && stack[stack.length - 1] !== '(') {
+                temp += stack.pop();
+            }
+            stack.pop();
+            for (let c of temp) {
+                stack.push(c);
+            }
+        } else {
+            stack.push(char);
+        }
+    }
+
+    return stack.join('');
+};
