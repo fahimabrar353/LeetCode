7a9c7124fc775bd5a8db42243333056ee216766e
fahimabrar353 <47246463+fahimabrar353@users.noreply.github.com>
2024-08-27 01:55:35 +0600

    Time: 76 ms (36.45%), Space: 54.2 MB (30.05%) - LeetHub

---
+++ 0590-n-ary-tree-postorder-traversal/0590-n-ary-tree-postorder-traversal.js
@@ -0,0 +1,17 @@
+var postorder = function(root) {
+    const result = [];
+    
+    function traverse(node) {
+        if (!node) return;
+        
+        for (let child of node.children) {
+            traverse(child);
+        }
+        
+        result.push(node.val);
+    }
+    
+    traverse(root);
+    
+    return result;
+};
