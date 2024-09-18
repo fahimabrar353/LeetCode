fecded6477c745c881b9a9e814797e028921d90c
fahimabrar353 <47246463+fahimabrar353@users.noreply.github.com>
2024-09-19 02:02:23 +0600

    Time: 58 ms (50.18%), Space: 50.9 MB (46.77%) - LeetHub

---
+++ 0179-largest-number/0179-largest-number.js
@@ -0,0 +1,6 @@
+var largestNumber = function(nums) {
+    nums = nums.map(String);
+    nums.sort((a, b) => (b + a) - (a + b));
+    let result = nums.join('');
+    return result[0] === '0' ? '0' : result;
+};
